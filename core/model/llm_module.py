256b3f55adb01375df6c53cafa2baf260fc5dbaa
Dania592 <118030608+Dania592@users.noreply.github.com>
2025-02-28 00:02:46 +0100

    Implémentation du module RAG

---
+++ core/model/llm_module.py
@@ -0,0 +1,39 @@
+from transformers import pipeline
+
+from core.model.rag_module import RagModule
+
+
+class LlmModule:
+    def __init__(self):
+        self.model = pipeline("text2text-generation", model="google/flan-t5-large")
+        self.rag = RagModule()
+
+    def build_prompt(self, context, question):
+        prompt = f"""
+    Tu es un assistant intelligent qui répond avec précision aux questions en utilisant les informations fournies. 
+    Utilise uniquement le contexte donné ci-dessous pour formuler ta réponse.
+
+    ### Contexte :
+    {context}
+
+    ### Question :
+    {question}
+
+    ### Réponse :
+    """
+        return prompt
+
+    def generate_response(self, question, faiss_path):
+        results = self.rag.search_faiss(question, faiss_path)
+        context = ""
+        for result in results:
+            context += result.page_content + "\n"
+
+        prompt = f"Réponds à la question en utilisant ces informations :\n\n{context}\n\nQuestion: {question}\nRéponse:"
+        # prompt = build_prompt(context, question)
+
+        responses = self.model(prompt, max_length=200, do_sample=True, temperature=0.7)
+
+        return responses
+
+
